33697d4c8472d3b12ef1b45725e6d844bb2dd4a9
Bertin-Telou <132468964+Bertin-Telou@users.noreply.github.com>
2025-09-03 18:06:52 +0000

    Update config.php

--- liyokgreen_dashboard/config.php
+++ liyokgreen_dashboard/config.php
@@ -1,1 +1,14 @@
+<?php
+$host = "localhost";
+$dbname = "liyokgreen";  // ⚠️ à modifier
+$username = "root";           // ⚠️ à modifier si besoin
+$password = "";               // ⚠️ mot de passe MySQL si configuré
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur connexion : " . $e->getMessage());
+}
+?>
 
