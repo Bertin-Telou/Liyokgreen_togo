4e7edc112c7a99808fe2a84a5277295b1717efdf
Bertin-Telou <132468964+Bertin-Telou@users.noreply.github.com>
2025-09-03 18:27:41 +0000

    Update config.php

--- liyokgreen_dashboard/config.php
+++ liyokgreen_dashboard/config.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$dbname = "liyokgreen";  // ⚠️ à modifier
-$username = "root";           // ⚠️ à modifier si besoin
-$password = "";               // ⚠️ mot de passe MySQL si configuré
+$dbname = "liyokgreen";  
+$username = "root";           
+$password = "";               
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
